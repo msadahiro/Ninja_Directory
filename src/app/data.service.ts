f3ba273e6491b75a90d709fb9c7b4019c296d098
Michael Sadahiro <michaelsadahiro@gmail.com>
2017-04-10 09:08:26 -0700

    connecting to firebase

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,7 +8,7 @@ export class DataService {
   constructor(private http: Http) { }
 
   fetchData(){
-    return this.http.get('/ninjas.json').map(
+    return this.http.get('https://ninja-angular2-66181.firebaseio.com/.json').map(
       (response) => response.json()
     );
   }
